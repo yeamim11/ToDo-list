cc3dc8177034df08ed65a9acad1a08ff2c4ad489
yeamim11 <165083549+yeamim11@users.noreply.github.com>
2024-05-24 02:19:20 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,89 @@
+// let input = document.getElementById("inp");
+// let text = document.querySelector(".text");
+
+// function add(){
+//     if(input.value == ""){
+//         alert("please enter task")
+//     }else{
+//         let newEle = document.createElement("ul");
+//         newEle.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
+//         text.appendChild(newEle);
+//         saveData();
+//         input.value="";
+
+//         newEle.querySelector("i").addEventListener("click", remove );
+//         function remove(){
+//             newEle.remove();
+//         }
+//         saveData();
+//     }
+// }
+
+
+// function saveData(){
+//     localStorage.setItem("data", text.value);
+
+// }
+// function showTask(){
+//     text.innerHTML = localStorage.getItem("deta");
+// }
+// showTask()
+
+
+
+
+
+
+
+
+
+
+
 let input = document.getElementById("inp");
 let text = document.querySelector(".text");
 
-function add(){
-    if(input.value == ""){
-        alert("please enter task")
-    }else{
+// Load tasks from local storage when the page loads
+document.addEventListener("DOMContentLoaded", loadData);
+
+function add() {
+    if (input.value == "") {
+        alert("Please enter a task");
+    } else {
         let newEle = document.createElement("ul");
         newEle.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
         text.appendChild(newEle);
-        input.value="";
-        newEle.querySelector("i").addEventListener("click", remove );
-        function remove(){
-            newEle.remove()
+        saveData();
+        input.value = "";
+
+        newEle.querySelector("i").addEventListener("click", remove);
+        function remove() {
+            newEle.remove();
+            saveData();
         }
     }
 }
+
+function saveData() {
+    const tasks = document.querySelectorAll(".text ul");
+    const taskList = [];
+    tasks.forEach(task => {
+        taskList.push(task.textContent);
+    });
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+}
+
+function loadData() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(task => {
+        let newEle = document.createElement("ul");
+        newEle.innerHTML = `${task} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+
+        newEle.querySelector("i").addEventListener("click", remove);
+        function remove() {
+            newEle.remove();
+            saveData();
+        }
+    });
+}
+
